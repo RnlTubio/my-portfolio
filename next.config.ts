abd38d5c460b7a465094371444a5709392994de9
Ronel Tubio <roneltubio781@gmail.com>
2025-12-03 14:30:16 +0800

    301 issue

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/my-portfolio",
+        destination: "/",
+        permanent: true, // 301 Redirect - tells Google the page has moved permanently
+      },
+    ];
+  },
   images: {
     qualities: [75, 90],
   },
